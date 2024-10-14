41113b7a4f82c2d5458bf31678c1209efe0b8a01
LiceAwz <liceawazu@gmail.com>
2024-10-14 12:59:33 -0300

    adciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para treinamento de programação do zero do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return<h1>Primeiro teste</h1>
+  return <h1>Primeiro teste</h1>;
 }
 
 export default Home;
